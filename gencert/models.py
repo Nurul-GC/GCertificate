2e4291b76fb45544e9619c4abcffa2ffa00a933a
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-05 18:14:44 +0200

    update gencert/models.py

--- gencert/models.py
+++ gencert/models.py
@@ -3,10 +3,23 @@ from django.db import models
 
 # Create your models here.
 class Certificate(models.Model):
-    TYPES = (('Certificado de Formação', 'Certificado de Formação'),
-             ('Certificado de Participação', 'Certificado de Participação'))
+    LANG = (
+        ('English', 'English'),
+        ('Português', 'Português')
+    )
+    TYPES = (
+        ('Training Certificate', 'Training Certificate'),
+        ('Certificado de Formação', 'Certificado de Formação'),
+        ('Certificado de Participação', 'Certificado de Participação'),
+        ('Certificate of Participation', 'Certificate of Participation')
+    )
     logo = models.ImageField()
-    title = models.CharField(max_length=50)
-    type = models.CharField(choices=TYPES)
-    company = models.CharField(max_length=50)
-    student = models.CharField(max_length=50)
+    language = models.CharField(choices=LANG)
+    document_title = models.CharField(max_length=50)
+    document_type = models.CharField(choices=TYPES)
+    company_name = models.CharField(max_length=50)
+    student_name = models.CharField(max_length=50)
+    description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return f"{self.student_name} certified by {self.company_name}"
