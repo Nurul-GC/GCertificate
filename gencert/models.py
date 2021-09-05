d566d0490a72efb456acf6e7a74fad3f71213233
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-05 04:36:55 +0200

    update gencert/models.py

--- gencert/models.py
+++ gencert/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Certificate(models.Model):
+    TYPES = (('Certificado de Formação', 'Certificado de Formação'),
+             ('Certificado de Participação', 'Certificado de Participação'))
+    logo = models.ImageField()
+    title = models.CharField(max_length=50)
+    type = models.CharField(choices=TYPES)
+    company = models.CharField(max_length=50)
+    student = models.CharField(max_length=50)
