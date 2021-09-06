9e62a324a5f54ec08f91a32d213cdb422735c22a
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-06 23:38:52 +0200

    update gencert/views.py

--- gencert/views.py
+++ gencert/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return render(request=request, template_name='index.html')
+
+
+def tc(request):
+    return render(request=request, template_name='training_certificate.html')
+
+
+def cp(request):
+    return render(request=request, template_name='certificate_participation.html')
