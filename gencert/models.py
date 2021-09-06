b8f7dd7b8a2d8494b7342f53a7d495b2303be72b
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-06 23:39:26 +0200

    update gencert/models.py

--- gencert/models.py
+++ gencert/models.py
@@ -2,7 +2,30 @@ from django.db import models
 
 
 # Create your models here.
-class Certificate(models.Model):
+class CP(models.Model):
+    """certificate of perticipation model"""
+    LANG = (
+        ('English', 'English'),
+        ('Português', 'Português')
+    )
+    TYPES = (
+        ('Certificado de Participação', 'Certificado de Participação'),
+        ('Certificate of Participation', 'Certificate of Participation')
+    )
+    logo = models.ImageField()
+    language = models.CharField(choices=LANG, max_length=10, help_text='Escolha o idioma do certificado!')
+    document_title = models.CharField(max_length=50, help_text='Digite um titulo para o documento!')
+    document_subject = models.CharField(choices=TYPES, max_length=30, help_text='Escolha o tema do certificado!')
+    company_name = models.CharField(max_length=50, help_text='Digite o nome da instituição emissora!')
+    student_name = models.CharField(max_length=50, help_text='Digite o nome do estudante!')
+    description = models.TextField(max_length=1000, help_text='Digite o conteudo descritivo e motivo desta certificação!')
+
+    def __str__(self):
+        return f"{self.student_name.name} certified by {self.company_name.name}"
+
+
+class TC(models.Model):
+    """training certificate model"""
     LANG = (
         ('English', 'English'),
         ('Português', 'Português')
@@ -10,16 +33,15 @@ class Certificate(models.Model):
     TYPES = (
         ('Training Certificate', 'Training Certificate'),
         ('Certificado de Formação', 'Certificado de Formação'),
-        ('Certificado de Participação', 'Certificado de Participação'),
-        ('Certificate of Participation', 'Certificate of Participation')
     )
     logo = models.ImageField()
-    language = models.CharField(choices=LANG)
-    document_title = models.CharField(max_length=50)
-    document_type = models.CharField(choices=TYPES)
-    company_name = models.CharField(max_length=50)
-    student_name = models.CharField(max_length=50)
-    description = models.TextField(max_length=1000)
+    language = models.CharField(choices=LANG, max_length=10, help_text='Escolha o idioma do certificado!')
+    document_title = models.CharField(max_length=50, help_text='Digite um titulo para o documento!')
+    document_subject = models.CharField(choices=TYPES, max_length=30, help_text='Escolha o tema do certificado!')
+    company_name = models.CharField(max_length=50, help_text='Digite o nome da instituição emissora!')
+    student_name = models.CharField(max_length=50, help_text='Digite o nome do estudante!')
+    course_name = models.CharField(max_length=50, help_text='Digite o nome do curso!')
+    description = models.TextField(max_length=1000, help_text='Digite o conteudo descritivo e motivo desta certificação!')
 
     def __str__(self):
-        return f"{self.student_name} certified by {self.company_name}"
+        return f"{self.student_name.name} certified by {self.company_name.name}"
