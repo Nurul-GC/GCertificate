6ad514e3708e40b439e7d21d951fdd495e9144cb
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-07 19:41:02 +0200

    update gencert/tests.py

--- gencert/tests.py
+++ gencert/tests.py
@@ -10,7 +10,7 @@ class GenCertTest(unittest.TestCase):
         certificate.language = "English"  # Português
         certificate.title = "teste_en"
         certificate.type = "Certificate of Participation"  # Certificado de Participação
-        certificate.logo = "../staticfiles/img/favicon/favicon-192x192.png"
+        certificate.logo = "../static/img/favicon/favicon-192x192.png"
         certificate.company_name = "ArtesGC Inc"
         certificate.student_name = "Nurul Hosny Gaspar de Carvalho"
         certificate.description = "Congratulations,\nThank you!"  # Parabens,\nObrigado!
