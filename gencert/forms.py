4f09d56cb691ab6ca188339c845e168aeee46b8e
Nurul-GC <nuruldecarvalho@gmail.com>
2021-09-07 21:47:03 +0200

    update gencert/forms.py

--- gencert/forms.py
+++ gencert/forms.py
@@ -5,12 +5,12 @@ from gencert.models import CP, TC
 class CpForm(forms.ModelForm):
     class Meta:
         model = CP
-        fields = ['logo', 'language', 'documente_title', 'description',
-                  'document_subject', 'company_name', 'student_name']
+        fields = ['logo', 'language', 'document_title', 'document_subject',
+                  'company_name', 'student_name', 'description']
 
 
 class TcForm(forms.ModelForm):
     class Meta:
         model = TC
-        fields = ['logo', 'language', 'documente_title', 'description',
-                  'document_subject', 'company_name', 'student_name']
+        fields = ['logo', 'language', 'document_title', 'document_subject',
+                  'company_name', 'student_name', 'course_name', 'description']
