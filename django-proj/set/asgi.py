1875e3770c1df08c801319ecf4956e46c512efa1
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-26 00:10:58 +0200

    update django-proj/set/*

--- django-proj/set/asgi.py
+++ django-proj/set/asgi.py
@@ -7,6 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
+# ******************************************************************************
+#  Copyright (c) 2021 Nurul-GC.                                                *
+# ******************************************************************************
+
 import os
 
 from django.core.asgi import get_asgi_application
