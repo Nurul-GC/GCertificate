8172b01a5fbda1eed584cd1a4d590180d6cb2c2f
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-26 00:11:10 +0200

    update django-proj/set/*

--- django-proj/set/wsgi.py
+++ django-proj/set/wsgi.py
@@ -7,6 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
+# ******************************************************************************
+#  Copyright (c) 2021 Nurul-GC.                                                *
+# ******************************************************************************
+
 import os
 
 from django.core.wsgi import get_wsgi_application
