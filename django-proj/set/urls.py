2d7170a6679ed7b2d92b30558b654cdff1e6a348
Nurul-GC <nuruldecarvalho@gmail.com>
2021-12-26 00:11:07 +0200

    update django-proj/set/*

--- django-proj/set/urls.py
+++ django-proj/set/urls.py
@@ -13,6 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# ******************************************************************************
+#  Copyright (c) 2021 Nurul-GC.                                                *
+# ******************************************************************************
+
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
